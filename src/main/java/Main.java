dcb19ae297bc8ab636af373d9b0f2d7836d1562e
LiuYi0206 <liuyi19910206>
2020-02-01 00:50:44 +0800

    观看时间

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
-import queue.ArrayQueue;
+import queue.LoopQueue;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayQueue<Integer> arrayQueue = new ArrayQueue<Integer>();
+        LoopQueue<Integer> queue = new LoopQueue<Integer>();
         for (int i = 0; i < 10 ; i++) {
-            arrayQueue.enqueue(i);
-            System.out.println(arrayQueue);
+            queue.enqueue(i);
+            System.out.println(queue);
             if (i % 3 == 2) {
-                arrayQueue.dequeue();
-                System.out.println(arrayQueue);
+                queue.dequeue();
+                System.out.println(queue);
             }
         }
     }
--- src/main/java/queue/LoopQueue.java
+++ src/main/java/queue/LoopQueue.java
@@ -4,25 +4,88 @@ public class LoopQueue<E> implements Queue<E> {
 
     private E[] data;
 
+    private int front, tail;
 
+    private int size;
+
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacoty() {
+        return data.length - 1;
+    }
 
     public int getSize() {
-        return 0;
+        return size;
     }
 
     public boolean isEmpty() {
-        return false;
+        return front == tail;
     }
 
     public void enqueue(E e) {
+        if ((tail + 1) % data.length == front) {
+            resize(getCapacoty() * 2);
+        }
 
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size ++;
+    }
+
+    private void resize(int newCapacoty) {
+        E[] newData = (E[]) new Object[newCapacoty + 1];
+
+        for (int i = 0; i < size ; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
     }
 
     public E dequeue() {
-        return null;
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Cannot dequeue. from an empty queue.");
+        }
+
+        E ret = data[front];
+        data [front] = null;
+        front = (front + 1) % data.length;
+        size --;
+        if (size == getCapacoty() / 4 && getCapacoty() / 2 != 0) {
+            resize(getCapacoty() / 2);
+        }
+        return ret;
     }
 
     public E getFront() {
-        return null;
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty.");
+        }
+        return data[front];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = front; i != tail; i = (i +1) % data.length) {
+            builder.append(data[i]);
+            if ((i + 1) % data.length != tail)
+                builder.append(", ");
+        }
+        return "Queue:" +
+                "capacity=" + getCapacoty() +
+                ", size=" + size +
+                ", data={" + builder.toString() +
+                '}' ;
     }
 }
